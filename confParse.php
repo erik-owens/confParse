ac3f8007a666ffe972c0a5300a13c29ef78f776b
erik-owens <erik.ben.owens@gmail.com>
2014-10-01 15:06:25 -0700

    Added the allowed dir and host and user list files
    store authorized user and host list for validation.  This doesn’t
    store any auth data, they are just lists of users and hosts that are
    within a given application.

--- confParse.php
+++ confParse.php
@@ -7,14 +7,20 @@ session_start();
  * Time: 2:36 PM
  */
 
+$app_hosts = array(
+                         'test.com'
+                        ,'test2.com'
+                        ,'test3.com'
+                   );
 
-
+$app_users = array(
+                         'user'
+                        ,'apache'
+                        ,'developer_1'
+                        ,'developer_2'
+                   );
 
 $lines = file('site1.conf');
-
-    echo"<pre>";
-    var_dump($lines);
-    echo"</pre>";
 
 
 
@@ -35,44 +41,48 @@ foreach ( $lines as $line ) {
             echo"</pre>";*/
 
 
+if(false) {
 
 
+    switch ($column[0]) {
+        case 'host':
+            populate_session_var($columns);
+            break;
+        case 'server_id':
+            populate_session_var($columns);
+            break;
+        case 'server_load_alarm':
+            populate_session_var($columns);
+            break;
+        case 'user':
+            populate_session_var($columns);
+            break;
+        case 'verbose':
+            populate_session_var($columns);
+            break;
+        case 'test_mode':
+            populate_session_var($columns);
+            break;
+        case 'debug_mode':
+            populate_session_var($columns);
+            break;
+        case 'log_file_path':
+            populate_session_var($columns);
+            break;
+        case 'send_notifications':
+            populate_session_var($columns);
+            break;
+        default:
+    }
+}
 
-            switch ($column[0]) {
-                case 'host':
-                    populate_session_var($columns);
+            switch ($columns[0]) {
+                case "host":
+                    echo'populate host';
+                    populate_session_string_host($columns,$app_hosts);
                     break;
                 case 'server_id':
-                    populate_session_var($columns);
-                    break;
-                case 'server_load_alarm':
-                    populate_session_var($columns);
-                    break;
-                case 'user':
-                    populate_session_var($columns);
-                    break;
-                case 'verbose':
-                    populate_session_var($columns);
-                    break;
-                case 'test_mode':
-                    populate_session_var($columns);
-                    break;
-                case 'debug_mode':
-                    populate_session_var($columns);
-                    break;
-                case 'log_file_path':
-                    populate_session_var($columns);
-                    break;
-                case 'send_notifications':
-                    populate_session_var($columns);
-                    break;
-                default:
-            }
-
-
-            switch ($column[0]) {
-                case 'host':
-                case 'server_id':
+                    //populate_session_int($columns);
                 case 'server_load_alarm':
                 case 'user':
                 case 'log_file_path':
@@ -86,40 +96,67 @@ foreach ( $lines as $line ) {
                     break;
             }
 
-
-/*
-host
-server_id
-server_load_alarm
-case 'user':
-case 'verbose':
-case 'test_mode':
-case 'debug_mode':
-case 'log_file_path':
-case 'send_notifications':
-*/
-
-
         }
     }
 } // foreach $line end
 
 
+dump_session_conf_data();
+
+
+
+
+function populate_session_string_host($host, $hosts){
+
+    if ( in_array($host[1],$hosts) ){
+        $_SESSION['conf']['host'] = $host[1];
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+
 function populate_session_string($columns){
     if(isset($columns[0]) && isset($columns[1])) {
-        $_SESSION[$columns[0]] = $columns[1];
-        return true;
+        $_SESSION['conf'][$columns[0]] = $columns[1];
+        //return true;
     }else{
-        return false;
+        //return false;
     }
 }
 
 
 function populate_session_bool($columns){
-    if(isset($columns[0]) && isset($columns[1])) {
-        $_SESSION[$columns[0]] = $columns[1];
+
+        if ( preg_match('/true|yes|on/',$columns[1]) ){
+            $_SESSION['conf'][$columns[0]] = 'true';
+        } else {
+            $_SESSION['conf'][$columns[0]] = 'false';
+        }
+
+
+
+        // Set the $_SESSION var to true or false
+        $_SESSION[$columns[0]] = $bool;
+
+}
+
+function populate_session_int($host, $allowed_hosts){
+
+    if ( in_array($host[1],$allowed_hosts) ){
+        $_SESSION['conf']['host'] = $host[1];
         return true;
-    }else{
+    } else {
         return false;
     }
+
+
+}
+
+function dump_session_conf_data(){
+    echo'<pre>';
+    print_r($_SESSION);
+    echo'</pre>';
 }
