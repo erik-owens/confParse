429f92d71d7d1374a8d08c908cdaddbb58194080
erik-owens <erik.ben.owens@gmail.com>
2014-10-03 12:09:52 -0700

    fixing file naming issue

--- test.php
+++ test.php
@@ -6,10 +6,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-require_once('parseConf.php');
+require_once('confParse.php');
 
 //Test for site1.conf
-$conf_handler = new parseConf();
+$conf_handler = new confParse();
+
 $error = $conf_handler->parse('./conf/site1.conf');
 
 if ( isset($error) && strlen($error) > 0 ) {
